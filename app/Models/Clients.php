2cf832c6ef1ebbc3fc896f1efa02b02f6319c058
Matias Bubello <matt.bubello@gmail.com>
2025-03-23 21:28:27 -0300

    added soft delete

--- app/Models/Clients.php
+++ app/Models/Clients.php
@@ -4,9 +4,12 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Clients extends Model
 {
+    use SoftDeletes;
+
     public $incrementing = true;
     public $timestamps = true;
     protected $table = 'clients';
