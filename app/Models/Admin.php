1b3a233d3fa9f8dc4e8425503fcd741f15978f74
Matias Bubello <matt.bubello@gmail.com>
2025-04-08 21:01:03 -0300

    seeder

--- app/Models/Admin.php
+++ app/Models/Admin.php
@@ -2,12 +2,16 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
 use Illuminate\Support\Facades\Session;
 
 class Admin extends Authenticatable
 {
+    use HasFactory, Notifiable;
+
     protected $table = 'admins';
 
     protected $primaryKey = 'id';
