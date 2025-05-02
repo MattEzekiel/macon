c3c8eb0f8617e424cdbbc204bff1eeca82aba45c
Matias Bubello <matt.bubello@gmail.com>
2025-05-02 17:10:38 -0300

    Files height

--- resources/views/admin/files/forms/name-file.blade.php
+++ resources/views/admin/files/forms/name-file.blade.php
@@ -6,7 +6,7 @@
     <div class="flex flex-wrap justify-items-center items-center gap-5 mt-5">
         @foreach($files as $file)
             <div class="flex-1 border rounded shadow border-gray-700 min-w-3xs">
-                <object class="aspect-square w-full mb-3.5" data="{{ asset($file->file_url) }}"></object>
+                <object class="aspect-square w-full mb-3.5 max-h-80" data="{{ asset($file->file_url) }}"></object>
                 <x-forms.floating-input
                         type="text"
                         name="file_names[]"
